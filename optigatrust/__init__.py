8280d04e7e9bcf3a70b6d0f754e607533eacb587
Artem Yushev <12692378+ayushev@users.noreply.github.com>
2021-02-01 00:26:11 +0100

    remove x509 file from init

--- optigatrust/__init__.py
+++ optigatrust/__init__.py
@@ -1,10 +1,10 @@
 from .version import __version__, __version_info__
+
 __all__ = [
     '__version__',
     '__version_info__',
     'core',
     'asymmetric',
     'cert',
-    'transfer',
-    'x509'
+    'transfer'
 ]
