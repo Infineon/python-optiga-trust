30aee6a89f17a9b291680f24a0cf978ab6777ef8
Artem Yushev <12692378+ayushev@users.noreply.github.com>
2021-02-12 18:03:06 +0100

    Rename asymetric to crypto

--- optigatrust/crypto.py
+++ optigatrust/crypto.py
@@ -25,25 +25,25 @@ from ctypes import *
 import warnings
 import hashlib
 
-from optigatrust import core
-from optigatrust.const import x, m1, m3, charge
+import optigatrust as optiga
 
-__all__ = [
-    'EccKey',
-    'RsaKey',
-    'EcdsaSignature',
-    'RsaPkcs1v15Signature',
+__all__  = [
+    'random',
+    'ECCKey',
+    'RSAKey',
+    'ECDSASignature',
+    'PKCS1v15Signature',
 ]
 
 
 def _str2curve(curve_str, return_value=False):
     _map = {
-        'secp256r1': m3.Curves.SEC_P256R1,
-        'secp384r1': m3.Curves.SEC_P384R1,
-        'secp521r1': m3.Curves.SEC_P521R1,
-        'brainpoolp256r1': m3.Curves.BRAINPOOL_P256R1,
-        'brainpoolp384r1': m3.Curves.BRAINPOOL_P384R1,
-        'brainpoolp512r1': m3.Curves.BRAINPOOL_P512R1
+        'secp256r1': optiga.enums.m3.Curves.SEC_P256R1,
+        'secp384r1': optiga.enums.m3.Curves.SEC_P384R1,
+        'secp521r1': optiga.enums.m3.Curves.SEC_P521R1,
+        'brainpoolp256r1': optiga.enums.m3.Curves.BRAINPOOL_P256R1,
+        'brainpoolp384r1': optiga.enums.m3.Curves.BRAINPOOL_P384R1,
+        'brainpoolp512r1': optiga.enums.m3.Curves.BRAINPOOL_P512R1
     }
     if curve_str in _map:
         if return_value:
@@ -54,6 +54,43 @@ def _str2curve(curve_str, return_value=False):
         raise ValueError('Your curve ({0}) not supported use one of these: {1}'.format(curve_str, _map.keys()))
 
 
+def random(n, trng=True):
+    """
+    This function generates a random number
+
+    :ivar n:
+        how much randomness to generate. Valid values are from 8 to 256
+    :vartype n: int
+
+    :ivar trng:
+        If True the a True Random Generator will be used, otherwise Deterministic Random Number Generator
+    :vartype trng: bool
+
+    :raises:
+        - TypeError - when any of the parameters are of the wrong type
+        - OSError - when an error is returned by the chip initialisation library
+
+    :returns:
+        Bytes object with randomness
+    """
+    ot = optiga.Chip()
+    api = ot.api
+
+    api.exp_optiga_crypt_random.argtypes = c_byte, POINTER(c_ubyte), c_ushort
+    api.exp_optiga_crypt_random.restype = c_int
+    p = (c_ubyte * n)()
+
+    if trng is True:
+        ret = api.exp_optiga_crypt_random(ot.rng.TRNG.value, p, len(p))
+    else:
+        ret = api.exp_optiga_crypt_random(ot.rng.DRNG.value, p, len(p))
+
+    if ret == 0:
+        return bytes(p)
+    else:
+        return bytes(0)
+
+
 class _Signature:
     def __init__(self, hash_alg: str, key_id: int, signature: bytes, algorithm: str):
         self.hash_alg = hash_alg
@@ -62,38 +99,58 @@ class _Signature:
         self.algorithm = algorithm
 
 
-class EcdsaSignature(_Signature):
+class ECDSASignature(_Signature):
     def __init__(self, hash_alg, key_id, signature):
         signature_algorithm_id = '%s_%s' % (hash_alg, 'ecdsa')
         super().__init__(hash_alg, key_id, signature, signature_algorithm_id)
 
 
-class RsaPkcs1v15Signature(_Signature):
+class PKCS1v15Signature(_Signature):
     def __init__(self, hash_alg, keyid, signature):
         signature_algorithm_id = '%s_%s' % (hash_alg, 'rsa')
         super().__init__(hash_alg, keyid, signature, signature_algorithm_id)
 
 
-class EccKey(core.Object):
-    def __init__(self, key_id: int, curve='secp256r1'):
-        super(EccKey, self).__init__(key_id)
-        self._curve = curve
-        self._pkey = None
-        id_ref = self.optiga.key_id
-        if key_id not in (id_ref.ECC_KEY_E0F0.value, id_ref.ECC_KEY_E0F1.value, id_ref.ECC_KEY_E0F2.value,
-                          id_ref.ECC_KEY_E0F3.value) and key_id not in self.optiga.session_id_values:
-            raise ValueError(
-                'Your key_id {0} can\'t be sued to generate an ECC Key'.format(hex(key_id))
-            )
-        self._key_id = key_id
+class _Key:
+    def __init__(self):
         self._key_usage = None
-        self._hash_alg = None
         self._pkey = None
         self._signature = None
+        self._key = None
 
     @property
     def pkey(self):
         return self._pkey
+
+    @property
+    def key(self):
+        """
+        In case a private key was requested during key pair generation
+        """
+        return self._key
+
+    @property
+    def key_usage(self):
+        return self._key_usage
+
+    @property
+    def signature(self):
+        return self._signature
+
+
+class ECCKey(optiga.Object, _Key):
+    def __init__(self, key_id: int, curve='secp256r1'):
+        super(ECCKey, self).__init__(key_id)
+        _Key.__init__(self)
+
+        self._curve = curve
+        id_ref = self._optiga.key_id
+        if key_id not in (id_ref.ECC_KEY_E0F0.value, id_ref.ECC_KEY_E0F1.value, id_ref.ECC_KEY_E0F2.value,
+                          id_ref.ECC_KEY_E0F3.value) and key_id not in self._optiga.session_id_values:
+            raise ValueError(
+                'Your key_id {0} can\'t be sued to generate an ECC Key'.format(hex(key_id))
+            )
+        self._hash_alg = None
 
     @property
     def curve(self):
@@ -103,29 +160,24 @@ class EccKey(core.Object):
     def hash_alg(self):
         return self._hash_alg
 
-    @property
-    def key_id(self):
-        return self._key_id
-
-    @property
-    def signature(self):
-        return self._signature
-
-    @property
-    def key_usage(self):
-        return self._key_usage
-
-    def generate(self, curve='secp256r1', key_usage=None):
+    def generate(self, curve='secp256r1', key_usage=None, export=False):
         """
         This function generates an ECC keypair, the private part is stored on the core based on the provided slot
 
-        :param curve:
+        :ivar curve:
             Curve name, should be one of supported by the chip curves. For instance m3 has
-            the widest range of supported algorithms: nistp256r1, nistp384r1, nistp521r1, bra
+            the widest range of supported algorithms: secp256r1, secp384r1, secp521r1, brainpoolp256r1,
+            brainpoolp384r1, brainpoolp512r1
+        :vartype curve: str
 
-        :param key_usage:
+        :ivar key_usage:
             Key usage defined per string. Can be selected as following:
             ['key_agreement', 'authentication', 'encryption', 'signature']
+        :vartype key_usage: str
+
+        :ivar export:
+            Indicates whether the private key should be exported
+        :vartype export: bool
 
         :raises:
             - TypeError - when any of the parameters are of the wrong type
@@ -135,13 +187,14 @@ class EccKey(core.Object):
             EccKey object or None
         """
         _allowed_key_usage = {
-            'key_agreement': self.optiga.key_usage.KEY_AGR,
-            'authentication': self.optiga.key_usage.AUTH,
-            'signature': self.optiga.key_usage.SIGN
+            'key_agreement': self._optiga.key_usage.KEY_AGR,
+            'authentication': self._optiga.key_usage.AUTH,
+            'signature': self._optiga.key_usage.SIGN
         }
         _key_usage = list()
+        priv_key = None
         if key_usage is None:
-            _key_usage = [self.optiga.key_usage.KEY_AGR, self.optiga.key_usage.SIGN]
+            _key_usage = [self._optiga.key_usage.KEY_AGR, self._optiga.key_usage.SIGN]
         else:
             for entry in key_usage:
                 if entry not in _allowed_key_usage:
@@ -150,30 +203,42 @@ class EccKey(core.Object):
                     )
                 _key_usage.append(_allowed_key_usage[entry])
 
-        print(_key_usage)
         c = _str2curve(curve, return_value=True)
-        if c not in self.optiga.curves_values:
+        if c not in self._optiga.curves_values:
             raise TypeError(
                 "object_id not found. \n\r Supported = {0},\n\r  "
-                "Provided = {1}".format(list(self.optiga.curves_values), c))
+                "Provided = {1}".format(list(self._optiga.curves_values), c))
 
-        self.optiga.api.exp_optiga_crypt_ecc_generate_keypair.argtypes = c_int, c_ubyte, c_bool, c_void_p, POINTER(
+        self._optiga.api.exp_optiga_crypt_ecc_generate_keypair.argtypes = c_int, c_ubyte, c_bool, c_void_p, POINTER(
             c_ubyte), POINTER(
             c_ushort)
-        self.optiga.api.exp_optiga_crypt_ecc_generate_keypair.restype = c_int
+        self._optiga.api.exp_optiga_crypt_ecc_generate_keypair.restype = c_int
 
         c_keyusage = c_ubyte(sum(map(lambda ku: ku.value, _key_usage)))
-        c_keyid = c_ushort(self.key_id)
-        p = (c_ubyte * 150)()
-        c_plen = c_ushort(len(p))
+        pkey = (c_ubyte * 150)()
+        c_plen = c_ushort(len(pkey))
 
-        ret = self.optiga.api.exp_optiga_crypt_ecc_generate_keypair(c, c_keyusage, 0, byref(c_keyid), p, byref(c_plen))
+        if export:
+            # https://github.com/Infineon/optiga-trust-m/wiki/Data-format-examples#RSA-Private-Key
+            key = (c_ubyte * (100 + 4))()
+        else:
+            key = byref(c_ushort(self.id))
 
-        pubkey = (c_ubyte * c_plen.value)()
-        memmove(pubkey, p, c_plen.value)
+        ret = self._optiga.api.exp_optiga_crypt_ecc_generate_keypair(c, c_keyusage, int(export), key, pkey, byref(c_plen))
+
+        if export:
+            priv_key = (c_ubyte * (100 + 4))()
+            memmove(priv_key, key, 100 + 4)
+            pub_key = (c_ubyte * c_plen.value)()
+            memmove(pub_key, pkey, c_plen.value)
+        else:
+            pub_key = (c_ubyte * c_plen.value)()
+            memmove(pub_key, pkey, c_plen.value)
 
         if ret == 0:
-            self._pkey = bytes(pubkey)
+            self._pkey = bytes(pub_key)
+            if export:
+                self._key = bytes(priv_key)
             self._key_usage = _key_usage
             self._curve = curve
             return self
@@ -204,9 +269,9 @@ class EccKey(core.Object):
         else:
             _d = data
 
-        self.optiga.api.exp_optiga_crypt_ecdsa_sign.argtypes = POINTER(c_ubyte), c_ubyte, c_ushort, POINTER(
+        self._optiga.api.exp_optiga_crypt_ecdsa_sign.argtypes = POINTER(c_ubyte), c_ubyte, c_ushort, POINTER(
             c_ubyte), POINTER(c_ubyte)
-        self.optiga.api.exp_optiga_crypt_ecdsa_sign.restype = c_int
+        self._optiga.api.exp_optiga_crypt_ecdsa_sign.restype = c_int
         _map = {
             'secp256r1': [hashlib.sha256, 32, 'sha256'],
             'secp384r1': [hashlib.sha384, 48, 'sha384'],
@@ -223,13 +288,13 @@ class EccKey(core.Object):
         # hash_algorithm = 'sha256'
         digest = (c_ubyte * param[1])(*param[0](_d).digest())
         # We reserve two extra bytes for nistp512r1 curve, shich has signature r/s values longer than a hash size
-        s = (c_ubyte * ((param[1]*2 + 2) + 6))()
+        s = (c_ubyte * ((param[1] * 2 + 2) + 6))()
         hash_algorithm = param[2]
 
         c_slen = c_ubyte(len(s))
         self._hash_alg = hash_algorithm
 
-        ret = self.optiga.api.exp_optiga_crypt_ecdsa_sign(digest, len(digest), self.key_id, s, byref(c_slen))
+        ret = self._optiga.api.exp_optiga_crypt_ecdsa_sign(digest, len(digest), self.id, s, byref(c_slen))
 
         if ret == 0:
             signature = (c_ubyte * (c_slen.value + 2))()
@@ -237,49 +302,41 @@ class EccKey(core.Object):
             signature[1] = c_slen.value
             memmove(addressof(signature) + 2, s, c_slen.value)
 
-            return EcdsaSignature(hash_algorithm, self.key_id, bytes(signature))
+            return ECDSASignature(hash_algorithm, self.id, bytes(signature))
         else:
             raise IOError('Function can\'t be executed. Error {0}'.format(hex(ret)))
 
 
-class RsaKey(core.Object):
+class RSAKey(optiga.Object, _Key):
     def __init__(self, key_id: int):
-        super(RsaKey, self).__init__(key_id)
-
         if key_id != 0xe0fc and key_id != 0xe0fd:
             raise ValueError(
                 'key_id isn\'t supported should be either 0xe0fc, or 0xe0fd, you provided {0}'.format(hex(key_id))
             )
-        self._key_usage = None
         self._key_size = None
-        self._pkey = None
+        super(RSAKey, self).__init__(key_id)
+        _Key.__init__(self)
 
     @property
     def key_size(self):
         return self._key_size
 
-    @property
-    def pkey(self):
-        return self._pkey
-
-    @property
-    def key_usage(self):
-        return self._key_usage
-
-    def generate(self, key_size=1024, key_usage=None):
+    def generate(self, key_size=1024, key_usage=None, export=False):
         """
         This function generates an RSA keypair, the private part is stored on the core based on the provided slot
 
-        :param key_size:
+        :ivar key_size:
             Size of the key, can be 1024 or 2048
+        :vartype key_size: int
 
-        :param key_usage:
+        :ivar key_usage:
             Key usage defined per string. Can be selected as following:
             ['key_agreement', 'authentication', 'encryption', 'signature']
+        :vartype key_usage: list
 
-        :param key_usage:
-            A key usage indicator. The value should be the KeyUsage Enumeration. By default
-            [KEY_AGR, AUTH, ENCRYPT]
+        :ivar export:
+            Indicates whether the private key should be exported
+        :vartype export: bool
 
         :raises:
             - TypeError - when any of the parameters are of the wrong type
@@ -289,14 +346,15 @@ class RsaKey(core.Object):
             RsaKey object or None
         """
         _allowed_key_usages = {
-            'key_agreement': self.optiga.key_usage.KEY_AGR,
-            'authentication': self.optiga.key_usage.AUTH,
-            'encryption': self.optiga.key_usage.ENCRYPT,
-            'signature': self.optiga.key_usage.SIGN
+            'key_agreement': self._optiga.key_usage.KEY_AGR,
+            'authentication': self._optiga.key_usage.AUTH,
+            'encryption': self._optiga.key_usage.ENCRYPT,
+            'signature': self._optiga.key_usage.SIGN
         }
         _key_usage = list()
+        priv_key = None
         if key_usage is None:
-            _key_usage = [self.optiga.key_usage.KEY_AGR, self.optiga.key_usage.SIGN]
+            _key_usage = [self._optiga.key_usage.KEY_AGR, self._optiga.key_usage.SIGN]
         else:
             for entry in key_usage:
                 if entry not in _allowed_key_usages:
@@ -306,7 +364,7 @@ class RsaKey(core.Object):
                 _key_usage.append(_allowed_key_usages[entry])
 
         _bytes = None
-        api = self.optiga.api
+        api = self._optiga.api
 
         allowed_key_sizes = {1024, 2048}
         if key_size not in allowed_key_sizes:
@@ -326,19 +384,32 @@ class RsaKey(core.Object):
             rsa_header = b'\x30\x82\x01\x22\x30\x0d\x06\x09\x2a\x86\x48\x86\xf7\x0d\x01\x01\x01\x05\x00'
 
         c_keyusage = c_ubyte(sum(map(lambda ku: ku.value, _key_usage)))
-        c_keyid = c_ushort(self.id)
-        p = (c_ubyte * 320)()
-        c_plen = c_ushort(len(p))
 
-        ret = api.exp_optiga_crypt_ecc_generate_keypair(c_keytype, c_keyusage, 0, byref(c_keyid), p, byref(c_plen))
+        pkey = (c_ubyte * 320)()
+        if export:
+            # https://github.com/Infineon/optiga-trust-m/wiki/Data-format-examples#RSA-Private-Key
+            key = (c_ubyte * (100 + 4))()
+        else:
+            key = byref(c_ushort(self.id))
+        c_plen = c_ushort(len(pkey))
 
-        pubkey = (c_ubyte * c_plen.value)()
-        memmove(pubkey, p, c_plen.value)
+        ret = api.exp_optiga_crypt_ecc_generate_keypair(c_keytype, c_keyusage, int(export), key, pkey, byref(c_plen))
+
+        if export:
+            priv_key = (c_ubyte * (100 + 4))()
+            memmove(priv_key, key, 100 + 4)
+            pub_key = (c_ubyte * c_plen.value)()
+            memmove(pub_key, pkey, c_plen.value)
+        else:
+            pub_key = (c_ubyte * c_plen.value)()
+            memmove(pub_key, pkey, c_plen.value)
 
         if ret == 0:
-            self._pkey = rsa_header + bytes(pubkey)
+            self._pkey = rsa_header + bytes(pub_key)
             self._key_usage = _key_usage
             self._key_size = key_size
+            if export:
+                self._key = bytes(priv_key)
             return self
         else:
             raise IOError('Function can\'t be executed. Error {0}'.format(hex(ret)))
@@ -360,7 +431,7 @@ class RsaKey(core.Object):
         :returns:
             RsaPkcs1v15Signature object or None
         """
-        api = self.optiga.api
+        api = self._optiga.api
 
         if not isinstance(data, bytes) and not isinstance(data, bytearray):
             if isinstance(data, str):
@@ -395,6 +466,6 @@ class RsaKey(core.Object):
             signature = (c_ubyte * c_slen.value)()
             memmove(addressof(signature), s, c_slen.value)
             print(bytes(signature))
-            return RsaPkcs1v15Signature(hash_algorithm, self.id, bytes(signature))
+            return PKCS1v15Signature(hash_algorithm, self.id, bytes(signature))
         else:
             raise IOError('Function can\'t be executed. Error {0}'.format(hex(ret)))
