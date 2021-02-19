783ee884d857294b9251c242eedd6a759e216923
Artem Yushev <12692378+ayushev@users.noreply.github.com>
2021-02-19 18:50:50 +0100

    fix from_json function from objects which dont habe change ac

--- optigatrust/port.py
+++ optigatrust/port.py
@@ -31,6 +31,7 @@ import optigatrust as optiga
 __all__ = [
     'to_json',
     'from_json',
+    'from_json_path',
     'to_otc',
 ]
 
@@ -78,7 +79,7 @@ def to_json():
     output = dict()
     # Read metadata from available keys
     for oid in opt.key_id_values:
-        print('Reading: {0}'.format(hex(oid)))
+        #print('Reading: {0}'.format(hex(oid)))
         key = optiga.Object(oid)
         raw_meta = key.read_raw_meta().hex()
         if len(raw_meta) == 0:
@@ -90,7 +91,7 @@ def to_json():
         del key
 
     for oid in opt.object_id_values:
-        print('Reading: {0}'.format(hex(oid)))
+        #print('Reading: {0}'.format(hex(oid)))
         key = optiga.Object(oid)
         raw_meta = key.read_raw_meta().hex()
         try:
@@ -110,11 +111,82 @@ def to_json():
     return output
 
 
-def from_json(data, path=None):
+def from_json(data=None):
     """
     This function will take as an input your data and populate the chip with it, whatever is possible
 
     :param data: JSON string with the dump of the data
+
+    .. highlight:: python
+    .. code-block:: python
+
+        {
+            "e0f1":
+            {
+                "metadata":'200fc00101d001ffd301ffe00103e10121',
+                "pretty_metadata":
+                {
+                    "lcso": "creation",
+                    "change": "never",
+                    "execute": "never",
+                    "algorithm": "secp256r1",
+                    "key_usage": ['authentication', 'key_agreement']
+                }
+            },
+            "e0c2":
+            {
+                "metadata":"2009c4011bd001ffd10100",
+                "pretty_metadata":
+                {
+                    "max_size": 27,
+                    "change": "never",
+                    "read": "always"
+                }
+                "data":"cd16338201001c000500000a091b5c0007006200ad801010710809"
+            }
+        }
+
+    :raises:
+        - ValueError - when any of the parameters contain an invalid value
+        - TypeError - when any of the parameters are of the wrong type
+        - OSError - when an error is returned by the chip initialisation library
+    """
+    supermeta = data
+
+    # Iterate through the dictionary and check all keys and values one by one
+    for oid, content in supermeta.items():
+        # A flag which we use to identify whether metadata should be reverted for data population
+        print('Reading {0}'.format(oid))
+        metadata_changed = False
+        old_meta = {}
+        try:
+            # Initialize the object with the object ID in the config
+            obj = optiga.Object(int(oid, 16))
+
+            # try at first to write down the data, otherwise it wont be possible to do this later (maybe)
+            if 'data' in content:
+                # Some settings object don#t have change metadata tag, so we can't assume this
+                try:
+                    old_meta = {'change': obj.meta['change']}
+                except KeyError:
+                    pass
+                obj.meta = {'change': 'always'}
+                metadata_changed = True
+                obj.write(bytes.fromhex(content['data']))
+
+            # Wtite raw metadata, this would be the fastest way, don't forget to convert hexstring to bytes
+            obj.meta = content['pretty_metadata']
+
+        except (OSError, ValueError, TypeError):
+            print('Warning. Failed to update {0} metadata. Skipping'.format(oid))
+        else:
+            if metadata_changed:
+                obj.meta = old_meta
+
+
+def from_json_path(path):
+    """
+    This function will take as an input your data and populate the chip with it, whatever is possible
 
     :param path: path to the json file. The content should be formed like the following
 
@@ -152,37 +224,10 @@ def from_json(data, path=None):
         - TypeError - when any of the parameters are of the wrong type
         - OSError - when an error is returned by the chip initialisation library
     """
-    if path is not None:
-        abs_path = os.path.abspath(path)
-        with open(abs_path, 'r', encoding='utf8') as f:
-            supermeta = json.loads(f)
-    else:
-        supermeta = data
+    with open(path, 'r', encoding='utf8') as f:
+        supermeta = json.loads(f)
 
-    # Iterate through the dictionary and check all keys and values one by one
-    for oid, content in supermeta.items():
-        # A flag which we use to identify whether metadata should be reverted for data population
-        metadata_changed = False
-        old_meta = {}
-        try:
-            # Initialize the object with the object ID in the config
-            obj = optiga.Object(int(oid, 16))
-
-            # try at first to write down the data, otherwise it wont be possible to do this later (maybe)
-            if 'data' in content:
-                old_meta = {'change': obj.meta['change']}
-                obj.meta = {'change': 'always'}
-                metadata_changed = True
-                obj.write(bytes.fromhex(content['data']))
-
-            # Wtite raw metadata, this would be the fastest way, don't forget to convert hexstring to bytes
-            obj.meta = content['pretty_metadata']
-
-        except (OSError, ValueError, TypeError):
-            print('Warning. Failed to update {0} metadata. Skipping'.format(oid))
-        else:
-            if metadata_changed:
-                obj.meta = old_meta
+    from_json(supermeta)
 
 
 def _to_xml(meta):
