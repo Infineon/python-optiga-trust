aca3b3af99aef458e02c086d34e131a45f2fdd50
Artem Yushev <12692378+ayushev@users.noreply.github.com>
2021-02-20 21:11:51 +0100

    add mocking options

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import re
 import sys
 sys.path.insert(0, os.path.abspath('../optigatrust'))
+autodoc_mock_imports = ["cryptography", "asn1crypto", "oscrypto", "jinja2"]
 
 
 # -- Project information -----------------------------------------------------
